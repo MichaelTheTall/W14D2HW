d1231db8cc4f4e92cb1f3a4346dd856689b30670
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-31 00:42:10 +0000

    songbox now takes in and parses API

--- src/containers/SongBox.js
+++ src/containers/SongBox.js
@@ -6,18 +6,28 @@ class SongBox extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      songs: [],
-      currentSong: null
+      songs: []
     };
-    //TODO bind things here
   }
 
-  //TODO load API on componentDidMount
+  componentDidMount() {
+    const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json';
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
 
-  //TODO fill render
+    request.addEventListener("load", () => {
+      if (request.status !== 200) return;
+      const jsonString = request.responseText;
+      const data = JSON.parse(jsonString);
+      this.setState({songs: data.feed.entry})
+    });
+
+    request.send();
+  }
+
   render(){
     return (
-      <SongList />
+      <SongList songs={this.state.songs} />
     )
   }
 }
